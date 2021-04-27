f5a94f577e902d2102b56403cab5ccef422c6a9a
CptOblivion <captainoblivion@gmail.com>
2021-04-27 13:29:50 -0400

    error log is appended instead of overwritten, time/date stamp added. Error log is named based on program version number.

--- Debug.py
+++ Debug.py
@@ -1,9 +1,10 @@
 from Helpers import *
 from Settings import Settings
 import traceback
+import datetime
 
 class debug():
-    logfileName = 'Error_Log.txt'
+    logfileName = 'Error_Log.txt_'+VersionNumber
     #turning this to True will force the UI to always show Camera 2 and 3 as connected
     forceCameraConnection = False
     
@@ -18,11 +19,15 @@ class debug():
         #TODO: properly handle traceback
         print('crash error:')
 
-        with open(debug.logfileName, 'w') as logfile:
+        with open(debug.logfileName, 'a') as logfile:
             #traceback.print_exception(message[0], message[1], message[2], file=logfile)
+            logfile.write('\n\n'+debug.getDateTime()+'\n')
             traceback.print_exc()
             traceback.print_exc(file=logfile)
             logfile.close()
+    def getDateTime():
+        timestamp=datetime.datetime.now()
+        return (timestamp.strftime('%y/%m/%d %H:%M:%S'))
 class DummySSH():
     UseDummy=False
     dummyPresetData=('* PresetListResult Preset 1 CameraId: 1\n'
