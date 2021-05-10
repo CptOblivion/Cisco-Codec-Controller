a49977420a7447f80696d0841b643f9efb273ad2
CptOblivion <captainoblivion@gmail.com>
2021-05-09 20:57:23 -0400

    * Bugfix: Midi note on was using the wrong variable to check velocity

--- CameraController.py
+++ CameraController.py
@@ -1117,7 +1117,7 @@ class CameraController():
                                 inputRouting.bindCommand('midi', 'note', 'button',
                                                          (self.inputDevicesMidiNames[deviceIndex], channel, key))
                         else:
-                            state = command == eventNoteOn and event[1]>0 #1 for noteOn, 0 for noteOff, velocity 0 is always off
+                            state = command == eventNoteOn and event[2]>0 #1 for noteOn, 0 for noteOff, velocity 0 is always off
                             for bind in Settings.commandBinds['midi']['note']:
                                 if (checkInputValidity(bind) and bind.inputNumber == key):
                                     bind.midiDeviceLast=self.inputDevicesMidiNames[deviceIndex]
